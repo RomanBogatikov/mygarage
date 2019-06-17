cabe750b1bb21228262168dbdde4d4ea57bedc0c
RomanBogatikov <ivaroman2000@gmail.com>
2019-06-17 16:03:03 -0400

    implemented auto login when new user signs up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        # auto login when a new user signs up
+        session[:user_id] = @user.id
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
